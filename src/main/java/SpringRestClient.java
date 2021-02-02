b634d6e6d7de8e955c3c92886a6c87f492a408c2
Ondrushko1986 <Ondrushko1986@yandex.ru>
2021-02-03 00:05:15 +0300

    1

--- src/main/java/SpringRestClient.java
+++ src/main/java/SpringRestClient.java
@@ -9,16 +9,15 @@ public class SpringRestClient {
 
     private static final String URL_USERS = "http://91.241.64.178:7081/api/users";
     private static final String DELETE_URL_USER = "http://91.241.64.178:7081/api/users/3";
-    private static final String COOKIE_NAME = "Cookie";
-    private static final String COOKIE_VALUE = "JSESSIONID=73D7B37F9583EF3BF1FBBBD1DC5F6A7E; Path=/; HttpOnly";
+    private static final String HEADER_NAME = "Cookie";
+    private static final String HEADER_VALUE = "JSESSIONID=56EDEC71D73041FD4C99D423D4CFF46F; Path=/; HttpOnly";
 
     private static final RestTemplate restTemplate = new RestTemplate();
-
-
+    private static final HttpHeaders headers = new HttpHeaders();
 
     private void getUsers() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        headers.add(HEADER_NAME, HEADER_VALUE);
+        headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<String> entity = new HttpEntity<>("params", headers);
         ResponseEntity<String> result = restTemplate.exchange(URL_USERS, HttpMethod.GET, entity,
                 String.class);
@@ -26,38 +25,26 @@ public class SpringRestClient {
     }
 
     private void createUser() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(COOKIE_NAME, COOKIE_VALUE);
         User newUser = new User(3L, "James", "Brown", (byte) 12);
         HttpEntity<User> requestBody = new HttpEntity<>(newUser,headers);
-        ResponseEntity<User> result = restTemplate.postForEntity(URL_USERS, requestBody, User.class);
-        System.out.println("Status code:" + result.getStatusCode());
+        ResponseEntity<String> result = restTemplate.exchange(URL_USERS, HttpMethod.POST, requestBody, String.class);
+        System.out.println(result);
 
-        if (result.getStatusCode() == HttpStatus.OK) {
-            User user = result.getBody();
-            assert user != null;
-            System.out.println("(Client Side) User is created: "+ user.getId());
-        }
     }
 
-//    private void updateUser() {
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.add(COOKIE_NAME, COOKIE_VALUE);
-//        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-//        User updatedUser = new User(3L, "Thomas", "Shelby", (byte) 55);
-//        RestTemplate restTemplate = new RestTemplate();
-//        HttpEntity<User> requestBody = new HttpEntity<>(updatedUser, headers);
-//        restTemplate.put(URL_USERS, requestBody, User.class);
-//    }
-//
-//    private void deleteUser() {
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.add(COOKIE_NAME, COOKIE_VALUE);
-//        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-//        RestTemplate restTemplate = new RestTemplate();
-//        HttpEntity<String> requestBody = new HttpEntity<>("params", headers);
-//        restTemplate.delete(DELETE_URL_USER, requestBody);
-//    }
+    private void updateUser() {
+        User updatedUser = new User(3L, "Thomas", "Shelby", (byte) 55);
+        HttpEntity<User> requestBody = new HttpEntity<>(updatedUser, headers);
+        ResponseEntity<String> result = restTemplate.exchange(URL_USERS, HttpMethod.PUT, requestBody, String.class);
+        System.out.println(result);
+    }
+
+    private void deleteUser() {
+        headers.add(HEADER_NAME, HEADER_VALUE);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        HttpEntity<String> requestBody = new HttpEntity<>("params", headers);
+        restTemplate.exchange(DELETE_URL_USER, HttpMethod.DELETE, requestBody, String.class);
+    }
 
     public static void main(String[] args) {
 
@@ -67,9 +54,9 @@ public class SpringRestClient {
 
         springRestClient.createUser();
 
-//        springRestClient.updateUser();
-//
-//        springRestClient.deleteUser();
+        springRestClient.updateUser();
+
+        springRestClient.deleteUser();
     }
 
 
